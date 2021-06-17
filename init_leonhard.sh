feb332a64fa8c7e3234c99b4ef4271df6e4a3807
wujiduan <51787079+wujiduan@users.noreply.github.com>
2021-06-17 09:47:35 +0200

    Update init_leonhard.sh

--- init_leonhard.sh
+++ init_leonhard.sh
@@ -1,5 +1,4 @@
 module load gcc/6.3.0 python_gpu/3.7.1 cuda/10.1.243 cudnn/7.6.4 eth_proxy
-python -m venv venv
 source venv/bin/activate
 pip install --upgrade pip setuptools wheel
 pip3 install -r requirements_leonhard.txt
