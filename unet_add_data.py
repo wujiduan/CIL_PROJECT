ded10ea028d5d3b058a4925f3f7b500a31c8d579
Silvia1019 <84081909+Silvia1019@users.noreply.github.com>
2021-07-31 14:45:25 +0100

    Add files via upload

---
+++ unet_add_data.py
@@ -0,0 +1,247 @@
+import math
+import os
+import re
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+from random import sample
+from PIL import Image
+import copy
+
+# some constants
+PATCH_SIZE = 16  # pixels per side of square patches
+VAL_SIZE = 800  # size of the validation set (number of images)
+CUTOFF = 0.25  # minimum average brightness for a mask patch to be classified as containing road
+
+def load_all_from_path(path):
+    # loads all HxW .pngs contained in path as a 4D np.array of shape (n_images, H, W, 3)
+    # images are loaded as floats with values in the interval [0., 1.]
+    return np.stack([np.array(Image.open(f)) for f in sorted(glob(path + '/*.png'))]).astype(np.float32) / 255.
+
+import torch
+from torch import nn
+from torch.utils.tensorboard import SummaryWriter
+from tqdm.notebook import tqdm
+
+def np_to_tensor(x, device):
+    # allocates tensors from np.arrays
+    if device == 'cpu':
+        return torch.from_numpy(x).cpu()
+    else:
+        return torch.from_numpy(x).contiguous().pin_memory().to(device=device, non_blocking=True)
+
+def accuracy_fn(y_hat, y):
+    # computes classification accuracy
+    return (y_hat.round() == y.round()).float().mean()
+
+class ImageDataset(torch.utils.data.Dataset):
+    # dataset class that deals with loading the data and making it available by index.
+
+    def __init__(self, path, device, use_patches=True, resize_to=(400, 400)):
+        self.path = path
+        self.device = device
+        self.use_patches = use_patches
+        self.resize_to=resize_to
+        self.x, self.y, self.n_samples = None, None, None
+        self._load_data()
+
+    def _load_data(self):  # not very scalable, but good enough for now
+        self.x = load_all_from_path(os.path.join(self.path, 'images'))
+        self.y = load_all_from_path(os.path.join(self.path, 'groundtruth'))
+        if self.use_patches:  # split each image into patches
+            self.x, self.y = image_to_patches(self.x, self.y)
+        elif self.resize_to != (self.x.shape[1], self.x.shape[2]):  # resize images
+            self.x = np.stack([cv2.resize(img, dsize=self.resize_to) for img in self.x], 0)
+            self.y = np.stack([cv2.resize(mask, dsize=self.resize_to) for mask in self.y], 0)
+        self.x = np.moveaxis(self.x, -1, 1)  # pytorch works with CHW format instead of HWC
+        self.n_samples = len(self.x)
+        print("number of pictures", self.n_samples, len(self.x), len(self.y))
+
+    def _preprocess(self, x, y):
+        # to keep things simple we will not apply transformations to each sample,
+        # but it would be a very good idea to look into preprocessing
+        return x, y
+
+    def __getitem__(self, item):
+        return self._preprocess(np_to_tensor(self.x[item], self.device), np_to_tensor(self.y[[item]], self.device))
+    
+    def __len__(self):
+        return self.n_samples
+
+class Block(nn.Module):
+    # a repeating structure composed of two convolutional layers with batch normalization and ReLU activations
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.block = nn.Sequential(nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=3, padding=1),
+                                   nn.ReLU(),
+                                   nn.BatchNorm2d(out_ch),
+                                   nn.Conv2d(in_channels=out_ch, out_channels=out_ch, kernel_size=3, padding=1),
+                                   nn.ReLU())
+
+    def forward(self, x):
+        return self.block(x)
+
+        
+class UNet(nn.Module):
+    # UNet-like architecture for single class semantic segmentation.
+    def __init__(self, chs=(3,64,128,256,512,1024)):
+        super().__init__()
+        enc_chs = chs  # number of channels in the encoder
+        dec_chs = chs[::-1][:-1]  # number of channels in the decoder
+        self.enc_blocks = nn.ModuleList([Block(in_ch, out_ch) for in_ch, out_ch in zip(enc_chs[:-1], enc_chs[1:])])  # encoder blocks
+        self.pool = nn.MaxPool2d(2)  # pooling layer (can be reused as it will not be trained)
+        self.upconvs = nn.ModuleList([nn.ConvTranspose2d(in_ch, out_ch, 2, 2) for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])])  # deconvolution
+        self.dec_blocks = nn.ModuleList([Block(in_ch, out_ch) for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])])  # decoder blocks
+        self.head = nn.Sequential(nn.Conv2d(dec_chs[-1], 1, 1), nn.Sigmoid()) # 1x1 convolution for producing the output
+
+    def forward(self, x):
+        # encode
+        enc_features = []
+        for block in self.enc_blocks[:-1]:
+            x = block(x)  # pass through the block
+            enc_features.append(x)  # save features for skip connections
+            x = self.pool(x)  # decrease resolution
+        x = self.enc_blocks[-1](x)
+        # decode
+        for block, upconv, feature in zip(self.dec_blocks, self.upconvs, enc_features[::-1]):
+            x = upconv(x)  # increase resolution
+            x = torch.cat([x, feature], dim=1)  # concatenate skip features
+            x = block(x)  # pass through the block
+        return self.head(x)  # reduce to 1 channel
+
+
+def patch_accuracy_fn(y_hat, y):
+    # computes accuracy weighted by patches (metric used on Kaggle for evaluation)
+    h_patches = y.shape[-2] // PATCH_SIZE
+    w_patches = y.shape[-1] // PATCH_SIZE
+    patches_hat = y_hat.reshape(-1, 1, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE).mean((-1, -3)) > CUTOFF
+    patches = y.reshape(-1, 1, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE).mean((-1, -3)) > CUTOFF
+    return (patches == patches_hat).float().mean()
+
+def train(train_dataloader, eval_dataloader, model, loss_fn, metric_fns, optimizer, n_epochs):
+    # training loop
+    logdir = '/cluster/home/haishi/cil/CIL_PROJECT/tensorboard/net'
+    check_path = '/cluster/home/haishi/cil/CIL_PROJECT/checkpoint/checkpoint'
+    folder_name = '/cluster/home/haishi/cil/CIL_PROJECT'
+    writer = SummaryWriter(logdir)  # tensorboard writer (can also log images)
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    history = {}  # collects metrics at the end of each epoch
+
+    for epoch in range(n_epochs):  # loop over the dataset multiple times
+
+        # initialize metric list
+        metrics = {'loss': [], 'val_loss': []}
+        for k, _ in metric_fns.items():
+            metrics[k] = []
+            metrics['val_'+k] = []
+
+        pbar = tqdm(train_dataloader, desc=f'Epoch {epoch+1}/{n_epochs}')
+        # training
+        model.train()
+        for (x, y) in pbar:
+            optimizer.zero_grad()  # zero out gradients
+            y_hat = model(x)  # forward pass
+            loss = loss_fn(y_hat, y)
+            loss.backward()  # backward pass
+            optimizer.step()  # optimize weights
+
+            # log partial metrics
+            metrics['loss'].append(loss.item())
+            for k, fn in metric_fns.items():
+                metrics[k].append(fn(y_hat, y).item())
+            pbar.set_postfix({k: sum(v)/len(v) for k, v in metrics.items() if len(v) > 0})
+
+        # validation
+        model.eval()
+        with torch.no_grad():  # do not keep track of gradients
+            for (x, y) in eval_dataloader:
+                y_hat = model(x)  # forward pass
+                loss = loss_fn(y_hat, y)
+                #loss = soft_dice_loss(y_hat, y)
+
+                # log partial metrics
+                metrics['val_loss'].append(loss.item())
+                for k, fn in metric_fns.items():
+                    metrics['val_'+k].append(fn(y_hat, y).item())
+
+        # summarize metrics, log to tensorboard and display
+        history[epoch] = {k: sum(v) / len(v) for k, v in metrics.items()}
+        for k, v in history[epoch].items():
+          writer.add_scalar(k, v, epoch)
+        print(' '.join(['\t- '+str(k)+' = '+str(v)+'\n ' for (k, v) in history[epoch].items()]))
+
+        if history[epoch]['val_acc'] > best_acc:
+            # print(history[epoch]['val_acc'])
+            best_acc = history[epoch]['val_acc']
+            best_model_wts = copy.deepcopy(model.state_dict())
+            torch.save(model.state_dict(),
+                       folder_name + '/model/model_e_temp.pt')
+
+    print('Finished Training')
+    # plot loss curves
+    plt.plot([v['loss'] for k, v in history.items()], label='Training Loss')
+    plt.plot([v['val_loss'] for k, v in history.items()], label='Validation Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+    plt.show()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+train_dataset = ImageDataset('/cluster/home/haishi/cil/CIL_PROJECT/training',
+                             device,
+                             use_patches=False,
+                             resize_to=(400, 400))
+val_dataset = ImageDataset('/cluster/home/haishi/cil/CIL_PROJECT/validation',
+                           device,
+                           use_patches=False,
+                           resize_to=(400, 400))
+train_dataloader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=4,
+                                               shuffle=True)
+val_dataloader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=4,
+                                             shuffle=True)
+model = UNet().to(device)
+loss_fn = nn.BCELoss()
+metric_fns = {'acc': accuracy_fn, 'patch_acc': patch_accuracy_fn}
+optimizer = torch.optim.Adam(model.parameters())
+n_epochs = 60
+train(train_dataloader, val_dataloader, model, loss_fn, metric_fns, optimizer,
+      n_epochs)
+folder_name = '/cluster/home/haishi/cil/CIL_PROJECT'
+torch.save(
+    model.state_dict(), folder_name + '/model/model' + str(n_epochs) + '_val' + '.pt')
+
+
+# predict on test set
+def create_submission(labels, test_filenames, submission_filename):
+    test_path='/cluster/home/haishi/cil/CIL_PROJECT/test_images/test_images'
+    with open(submission_filename, 'w') as f:
+        f.write('id,prediction\n')
+        for fn, patch_array in zip(sorted(test_filenames), test_pred):
+            img_number = int(re.search(r"\d+", fn).group(0))
+            for i in range(patch_array.shape[0]):
+                for j in range(patch_array.shape[1]):
+                    f.write("{:03d}_{}_{},{}\n".format(img_number, j*PATCH_SIZE, i*PATCH_SIZE, int(patch_array[i, j])))
+
+test_path = '/cluster/home/haishi/cil/CIL_PROJECT/test_images/test_images'
+test_filenames = (glob(test_path + '/*.png'))
+test_images = load_all_from_path(test_path)
+batch_size = test_images.shape[0]
+size = test_images.shape[1:3]
+# we also need to resize the test images. This might not be the best ideas depending on their spatial resolution.
+test_images = np.stack([cv2.resize(img, dsize=(384, 384)) for img in test_images], 0)
+test_images = np_to_tensor(np.moveaxis(test_images, -1, 1), device)
+test_pred = [model(t).detach().cpu().numpy() for t in test_images.unsqueeze(1)]
+test_pred = np.concatenate(test_pred, 0)
+test_pred= np.moveaxis(test_pred, 1, -1)  # CHW to HWC
+test_pred = np.stack([cv2.resize(img, dsize=size) for img in test_pred], 0)  # resize to original shape
+# now compute labels
+test_pred = test_pred.reshape((-1, size[0] // PATCH_SIZE, PATCH_SIZE, size[0] // PATCH_SIZE, PATCH_SIZE))
+test_pred = np.moveaxis(test_pred, 2, 3)
+test_pred = np.round(np.mean(test_pred, (-1, -2)) > CUTOFF)
+create_submission(test_pred, test_filenames, submission_filename=folder_name + '/predict/unet_submission.csv')
